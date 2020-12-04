6a7e836fcc82b8dd77677081d5d8f93af35cffda
OByrnes <liviabyrnes@gmail.com>
2020-12-04 17:49:45 -0500

    step 1

--- connect-four.js
+++ connect-four.js
@@ -1,13 +1,23 @@
 import Game from './game.js';
 
 let game;
+function updateUI () {
+  if (!game){
+    document.getElementById('board-holder').classList.add('is-invisible')
+    
+  } else {
+    document.getElementById('board-holder').classList.remove('is-invisible')
+    document.getElementById('game-name').innerHTML=game.getName()
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
 
+  const player1 = document.getElementById('player-1-name');
+  const player2 = document.getElementById('player-2-name');
+  const gameButton = document.getElementById('new-game');
   document.getElementById('form-holder').addEventListener('keyup', event => {
-    const player1 = document.getElementById('player-1-name');
-    const player2 = document.getElementById('player-2-name');
-    const gameButton = document.getElementById('new-game');
+    
     console.log(player1.value)
     if (player1.value !== '' && player2.value !== '') {
       gameButton.disabled = false;
@@ -16,6 +26,18 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
   })
+
+  gameButton.addEventListener('click', (event) =>{
+
+    game= new Game (player1.value, player2.value)
+    player1.value= '';
+    player2.value = '';
+
+    gameButton.disabled= true;
+    updateUI()
+  })
+
+
 
 
 })
--- game.js
+++ game.js
@@ -3,10 +3,15 @@ export default class Game {
   constructor(player1, player2) {
     this.player1 = player1;
     this.player2 = player2;
+    this.currentPlayer = 1
   }
+  playInColumn(){}
+
   getName() {
     return `${this.player1} vs. ${this.player2}`
   }
+  
+
 }
 
 // export default Game;
